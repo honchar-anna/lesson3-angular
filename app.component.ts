f46bd8785f74c1639fc173217a7eb5108e8ea7bb
honchar-anna <114396684+honchar-anna@users.noreply.github.com>
2023-02-01 15:10:43 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'lesson3';
+  @ViewChild(ChildComponent) childTable!: ChildComponent;
+
+  public user = 'Ivan Ivanov';
+  public count = 0;
+  public taskName!: string;
+  public save = false;
+  public pushName!: string;
+  public getTask!: string;
+  public index!: number;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addName(): void {
+    this.taskName = this.pushName;
+    this.pushName = '';
+  }
+  getName(index: number): void {
+    this.save = true;
+    this.getTask = this.childTable.taskList[index].name;
+    this.index = index;
+  }
+  getlength(length: number) {
+    this.count = length;
+  }
+  reName(): void {
+    this.childTable.taskList[this.index].name = this.getTask;
+    this.getTask = '';
+    this.save = false;
+  }
+}
+
